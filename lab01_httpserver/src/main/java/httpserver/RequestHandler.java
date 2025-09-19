b48ff453e3387c6a1bde50ac40e092377d0ff818
Timur_Sibgatullin <trsibgatullin@kpfu.ru>
2025-09-19 21:51:43 +0300

    lab01

---
+++ lab01_httpserver/src/main/java/httpserver/RequestHandler.java
@@ -0,0 +1,64 @@
+package httpserver;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+public class RequestHandler {
+    final static Logger logger = LogManager.getLogger(RequestHandler.class);
+
+    public void handle(Socket clientSocket) {
+        try {
+            // Поток для чтения данных от клиента
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            // Читаем пакет от клиента
+            String lineOne = reader.readLine();
+            System.out.println(lineOne);
+            logger.debug(lineOne);
+            String[] components = lineOne.split(" ");
+
+            //TODO реализовать определение метода (GET, POST,...) для передачи как параметра в сервис
+            // http://localhost:8080/resource/part?name=tat&region=16
+            // URI /resource/part
+            //
+            // При наличии извлечь параметры и поместить в Map
+
+            String resource = components[1];
+            if (resource.equals("/shutdown")) {
+                logger.info("server stopped by client");
+                //break;
+            }
+            while (true) {
+                // Читаем пакет от клиента
+                String message = reader.readLine();
+                System.out.println(message);
+                logger.debug(message);
+
+                if (message.isEmpty()) {
+                    logger.debug("end of request header");
+                    OutputStream os = clientSocket.getOutputStream();
+                    logger.debug("outputStream" + os);
+                    IResourceService resourceService = Application.resourceMap.get(resource);
+                    if (resourceService != null) {
+                        resourceService.service("GET", null, os);
+                    } else {
+                        new NotFoundService().service("GET", null, os);
+                    }
+                    os.flush();
+                    logger.debug("outputStream" + os);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
